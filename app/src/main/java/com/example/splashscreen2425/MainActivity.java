6b7875582259ab001e592fa527de7ff59384e00c
joanmrt_ <jmartinezs@cifpfbmoll.eu>
2025-01-09 16:51:00 +0100

    Splash Screen actually works

--- app/src/main/java/com/example/splashscreen2425/MainActivity.java
+++ app/src/main/java/com/example/splashscreen2425/MainActivity.java
@@ -16,38 +16,8 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.splash);
-        TextView logo1 = (TextView) findViewById(R.id.TextViewTopTitle);
-        Animation fade1 = AnimationUtils.loadAnimation(this, R.anim.fade_in);
-        logo1.startAnimation(fade1);
+        setContentView(R.layout.activity_main);
 
-        TextView logo2 = (TextView) findViewById(R.id.TextViewBottomTitle);
-        Animation fade2 = AnimationUtils.loadAnimation(this, R.anim.fade_in2);
-        logo2.startAnimation(fade2);
 
-        Animation spinin = AnimationUtils.loadAnimation(this, R.anim.custom_anim);
-        LayoutAnimationController controller = new LayoutAnimationController(spinin);
-        TableLayout table = (TableLayout) findViewById(R.id.TableLayout01);
-        for (int i = 0; i < table.getChildCount(); i++) {
-            TableRow row = (TableRow) table.getChildAt(i);
-            row.setLayoutAnimation(controller);
-        }
-
-    }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-        // Stop the animation
-        TextView logo1 = (TextView) findViewById(R.id.TextViewTopTitle);
-        logo1.clearAnimation();
-        TextView logo2 = (TextView) findViewById(R.id.TextViewBottomTitle);
-        logo2.clearAnimation();
-
-        TableLayout table = (TableLayout) findViewById(R.id.TableLayout01);
-        for (int i = 0; i < table.getChildCount(); i++) {
-            TableRow row = (TableRow) table.getChildAt(i);
-            row.clearAnimation();
-        }
     }
 }
--- app/src/main/java/com/example/splashscreen2425/SplashActivity.java
+++ app/src/main/java/com/example/splashscreen2425/SplashActivity.java
@@ -4,16 +4,38 @@ import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.view.animation.LayoutAnimationController;
+import android.widget.TableLayout;
+import android.widget.TableRow;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-@SuppressLint("CustomSplashScreen")
+
 public class SplashActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.splash);
+
+        TextView logo1 = (TextView) findViewById(R.id.TextViewTopTitle);
+        Animation fade1 = AnimationUtils.loadAnimation(this, R.anim.fade_in);
+        logo1.startAnimation(fade1);
+
+        TextView logo2 = (TextView) findViewById(R.id.TextViewBottomTitle);
+        Animation fade2 = AnimationUtils.loadAnimation(this, R.anim.fade_in2);
+        logo2.startAnimation(fade2);
+
+        Animation spinin = AnimationUtils.loadAnimation(this, R.anim.custom_anim);
+        LayoutAnimationController controller = new LayoutAnimationController(spinin);
+        TableLayout table = (TableLayout) findViewById(R.id.TableLayout01);
+        for (int i = 0; i < table.getChildCount(); i++) {
+            TableRow row = (TableRow) table.getChildAt(i);
+            row.setLayoutAnimation(controller);
+        }
 
         new Handler().postDelayed(new Runnable() {
             @Override
@@ -23,6 +45,22 @@ public class SplashActivity extends AppCompatActivity {
                 startActivity(intent);
                 finish(); // Close the splash activity
             }
-        }, 2000); // Delay for 2 seconds (adjust as needed)
+        }, 5000); // Delay for 2 seconds (adjust as needed)
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        // Stop the animation
+        TextView logo1 = (TextView) findViewById(R.id.TextViewTopTitle);
+        logo1.clearAnimation();
+        TextView logo2 = (TextView) findViewById(R.id.TextViewBottomTitle);
+        logo2.clearAnimation();
+
+        TableLayout table = (TableLayout) findViewById(R.id.TableLayout01);
+        for (int i = 0; i < table.getChildCount(); i++) {
+            TableRow row = (TableRow) table.getChildAt(i);
+            row.clearAnimation();
+        }
     }
 }
