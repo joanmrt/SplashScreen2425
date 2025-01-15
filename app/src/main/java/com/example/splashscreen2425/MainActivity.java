f8e4f69295ca60304c2c4ddf9aafca548831d314
joanmrt_ <jmartinezs@cifpfbmoll.eu>
2025-01-15 16:36:55 +0100

    Main Menu Added

--- app/src/main/java/com/example/splashscreen2425/MainActivity.java
+++ app/src/main/java/com/example/splashscreen2425/MainActivity.java
@@ -6,6 +6,8 @@ import android.os.Bundle;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.view.animation.LayoutAnimationController;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TableLayout;
 import android.widget.TableRow;
 import android.widget.TextView;
@@ -18,6 +20,16 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        ListView menuList = (ListView) findViewById(R.id.listview1);
 
+        String[] items = { getResources().getString(R.string.menu_item_play),
+                getResources().getString(R.string.menu_item_scores),
+                getResources().getString(R.string.menu_item_settings),
+                getResources().getString(R.string.menu_item_help) };
+
+        ArrayAdapter<String> adapt = new ArrayAdapter<String>(this,
+                R.layout.menu_item, items);
+
+        menuList.setAdapter(adapt);
     }
 }
